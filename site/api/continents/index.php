3a5edb19651868adb95c1ae60850d448db7e214b
Andres <anondiaz@gmail.com>
2025-05-29 13:03:16 +0200

    In progress language finalizado

---
+++ site/api/continents/index.php
@@ -0,0 +1,4 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+
+require_once '../../pdo_bind_connection.php';
---
+++ site/api/languages/index.php
@@ -0,0 +1,108 @@
+<?php
+
+header('Content-Type: application/json; charset=utf-8');
+
+require_once '../../pdo_bind_connection.php';
+
+// En la ruta /api/languages deben aparecer la lista de idiomas del mundo en orden alfabético y sin repeticiones
+
+// En la ruta /api/languages?total=true debe aparece la lista de idiomas con el total de hablantes estimado,
+// en orden descendente según ese total. Es esta información:
+//   [
+//       { 'idioma' : 'Chinese', 'hablantes' : '1191843539'},
+//       { 'idioma' : 'Hindi', 'hablantes' : '405633070'},
+//       { 'idioma' : 'Spanish', 'hablantes' : '355029462'},
+//       { 'idioma' : 'English', 'hablantes' : '347077867'},
+//       { 'idioma' : 'Arabic', 'hablantes' : '233839239'},
+//       etc
+//   ]
+
+// En la ruta /api/languages?lang=nombre_idioma deben aparecer los países en los que se habla el idioma indicado,
+// y el total de hablantes en ese país. Por ejemplo:
+// /api/languages?lang=catalan
+//    Debemos obtener:
+//    [
+//        { 'pais' : 'Spain', 'hablantes' : '6665647'},
+//        { 'pais' : 'Andorra', 'hablantes' : '25194'}
+//    ] 
+
+if (!$_GET) {
+    // Preparar la consulta
+    $select = "SELECT language FROM countrylanguage 
+            GROUP BY language 
+            ORDER BY language ASC";
+    $prep = $pdo->prepare($select);
+    // // Vincular el parámetro
+    // $prep->bindValue(':country', $country, PDO::PARAM_STR);
+    // Ejecutar la consulta
+    $prep->execute();
+    // Obtener los resultados
+    $languages = $prep->fetchAll(PDO::FETCH_ASSOC);
+    // Comprobar si se han encontrado países
+    echo json_encode($languages, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+    exit();
+}
+
+if (isset($_GET['total'])) {
+
+    // Continuar con el manejo de la solicitud GET
+    if (empty($_GET['total'])) {
+        http_response_code(400); // Bad Request
+        echo json_encode(['error' => 'Parámetro incorrecto. Debe ser "total" con un estado'], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        exit();
+    }
+    if ($_GET['total'] == 'true') {
+        $select = "SELECT language, SUM(population) AS hablantes 
+            FROM countrylanguage 
+            JOIN country ON countrylanguage.CountryCode = country.Code 
+            GROUP BY language 
+            ORDER BY hablantes DESC";
+        $prep = $pdo->prepare($select);
+        // Ejecutar la consulta
+        $prep->execute();
+        // Obtener los resultados
+        $languages = $prep->fetchAll(PDO::FETCH_ASSOC);
+        // Comprobar si se han encontrado países
+        echo json_encode($languages, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        exit();
+    } else {
+        http_response_code(400); // Bad Request
+        echo json_encode(['error' => 'Parámetro incorrecto. Debe ser "total" con un estado adecuado'], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        exit();
+    }
+} elseif (isset($_GET['lang'])) {
+    if (empty($_GET['lang'])) {
+        http_response_code(400); // Bad Request
+        echo json_encode(['error' => 'Parámetro incorrecto. Debe ser "lang" con el nombre de un pais'], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        exit();
+    }
+    if ($_GET['lang']) {
+        $language = $_GET['lang'];
+        // Preparar la consulta
+        $select = "SELECT country.Name AS pais, SUM(country.Population) AS hablantes 
+            FROM countrylanguage 
+            JOIN country ON countrylanguage.CountryCode = country.Code 
+            WHERE countrylanguage.Language = :language 
+            GROUP BY country.Name";
+        $prep = $pdo->prepare($select);
+        // Vincular el parámetro
+        $prep->bindValue(':language', $language, PDO::PARAM_STR);
+        // Ejecutar la consulta
+        $prep->execute();
+        // Obtener los resultados
+        $languages = $prep->fetchAll(PDO::FETCH_ASSOC);
+        // Comprobar si se han encontrado países
+        if (empty($languages)) {
+            http_response_code(400); // Bad Request
+            echo json_encode(['error' => 'Parámetro "lang" demasiado corto, vacio o no existe.'], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+            exit();
+        }
+        echo json_encode($languages, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        exit();
+    }
+
+} else {
+    http_response_code(405); // Method Not Allowed
+    echo json_encode(['error' => 'Método no permitido'], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+    exit();
+}
